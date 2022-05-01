99a09e1696a72f0ed21858b97890af10f9e04e9e
siddheshsurve <sysurve24@gmail.com>
2022-05-01 10:54:04 +0530

    Refined and added all input formats

--- app.py
+++ app.py
@@ -12,22 +12,89 @@ st.title("Heart disease predication")
 
 def report():
     age = st.number_input("Age")
+
     s= st.radio("Sex",('male','female'))
     if s == 'male':
         sex=1
     else:
         sex=0
-    cp = st.number_input("Chest Pain")
+    
+    # cp = st.number_input("Chest Pain")
+    c= st.radio("Chest Pain",('0','1','2','3'))
+    if c == '0':
+        cp = 0
+    elif c == '1':
+        cp=1
+    elif c == '2':
+        cp=2
+    else :
+        cp=3
+    
     trestbps = st.number_input("TrestBPS")
+
     chol = st.number_input("Cholesterol")
-    fbs = st.number_input("FBS")
-    restecg = st.number_input("restecg")
+
+    # fbs = st.number_input("FBS")
+    f= st.radio("Fasting Blood Sugar",('Yes','No'))
+    if f == 'Yes':
+        fbs=1
+    else:
+        fbs=0
+    
+    # restecg = st.number_input("restecg")
+    recg= st.radio("Resting Electrocardiogram (ECG) Results",('Noraml','having ST-T wave abnormality (T wave inversions and/or ST elevation or depression of > 0.05 mV)', 'showing probable or definite left ventricular hypertrophy by Estes'))
+    if recg == 'Normal':
+        restecg=0
+    elif recg == 'having ST-T wave abnormality (T wave inversions and/or ST elevation or depression of > 0.05 mV)':
+        restecg=1
+    else :
+        restecg=2
+    
     thalach = st.number_input("thalach")
-    exang = st.number_input("Exang")
+
+    # exang = st.number_input("Exang")
+    exg= st.radio("Exercise Induced Angina",('Yes','No'))
+    if exg == 'Yes':
+        exang=1
+    else:
+        exang=0
+
     oldpeak = st.number_input("OldPeak")
-    slope = st.number_input("Slope")
-    ca = st.number_input("CA")
-    thal = st.number_input("thal")
+
+    # slope = st.number_input("Slope") 
+    slp= st.radio("Slope of the peak exercise ST Segment",('0','1','2','3'))
+    if slp == '0':
+        slope = 0
+    elif slp == '1':
+        slope=1
+    elif slp == '2':
+        slope=2
+    else :
+        slope=3
+
+    # ca = st.number_input("CA")
+    cA= st.radio("CA (Number of major vessels)",('0','1','2','3','4'))
+    if cA == '0':
+        ca = 0
+    elif cA == '1':
+        ca=1
+    elif cA == '2':
+        ca=2
+    elif cA =='3' :
+        ca=3
+    else :
+        ca=4
+
+    # thal = st.number_input("thal")
+    thl= st.radio("Thalassemia (thal)",('0','1','2','3'))
+    if thl == '0':
+        thal = 0
+    elif thl == '1':
+        thal=1
+    elif thl == '2':
+        thal=2
+    else :
+        thal=3
     
 
     user_report_data={
@@ -43,7 +110,7 @@ def report():
         'oldpeak':oldpeak,
         'slope':slope,
         'ca':ca,
-        'tghal':thal
+        'thal':thal
      }
     report_data=pd.DataFrame(user_report_data,index=[0])
     return report_data
